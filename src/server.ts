e0a2445b7ab78fd90b034bfb2165bb69e06c40e5
Lucas-Baptista <lucasferbap@gmail.com>
2026-05-12 20:54:47 -0300

    feat: initial commit

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(
+    'Servidor rodando em http://localhost:3000 🚀',
+  );
+});
