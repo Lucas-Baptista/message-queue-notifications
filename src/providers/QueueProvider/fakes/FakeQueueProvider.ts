8d5548daf34fb80cde840dc2d784d37963a2c3ab
Lucas-Baptista <lucasferbap@gmail.com>
2026-05-13 19:19:24 -0300

    feat(queue): Implementing a fake queue consume and connect functions

--- src/providers/QueueProvider/fakes/FakeQueueProvider.ts
+++ src/providers/QueueProvider/fakes/FakeQueueProvider.ts
@@ -1,10 +1,17 @@
 import IQueueProvider from '../models/IQueueProvider';
 
+interface IMessage {
+  queue: string;
+  message: unknown;
+}
+
 export default class FakeQueueProvider implements IQueueProvider {
-  public messages: unknown[] = [];
+  public messages: IMessage[] = [];
+
+  public connected = false;
 
   async connect(): Promise<void> {
-    // fake connect
+    this.connected = true;
   }
 
   async publish(
@@ -17,7 +24,28 @@ export default class FakeQueueProvider implements IQueueProvider {
     });
   }
 
-  async consume(): Promise<void> {
-    // fake consume
+  async consume(
+    queue: string,
+    callback: (message: any) => Promise<void>,
+  ): Promise<void> {
+    if (!this.connected) {
+      throw new Error(
+        'Queue provider is not connected',
+      );
+    }
+
+    setInterval(async () => {
+      const queueMessages = this.messages.filter(
+        (item) => item.queue === queue,
+      );
+
+      queueMessages.forEach(async (item) => {
+        await callback(item.message);
+      });
+
+      this.messages = this.messages.filter(
+        (item) => item.queue !== queue,
+      );
+    }, 1000);
   }
 }
